4c01289c6099b04cf5bdc32e9b05119e7015bb01
Carlos Torres <carlosandrestorres28@hotmail.com>
2018-08-22 23:00:47 -0500

    display content rigth

--- app/assets/javascripts/angular/controllers/reddit.top.controller.js
+++ app/assets/javascripts/angular/controllers/reddit.top.controller.js
@@ -1,5 +1,11 @@
 (function(){
-  var module = angular.module('TopEntries', []);
+  var module = angular.module('TopEntries', []).filter('moment', function () {
+	  return function (input, momentFn /*, param1, param2, ...param n */) {
+	    var args = Array.prototype.slice.call(arguments, 2),
+	      momentObj = moment(input);
+	    return momentObj[momentFn].apply(momentObj, args);
+	  };
+	});;
   
   module.controller('TopController', ["$scope", "$http", topController]);
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require rails-ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require moment
 
 //= require angular
 //= require angular/controllers/reddit.top.controller
